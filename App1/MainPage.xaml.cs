1d32ab266edf17f894cd15b7c439202957e48ea1
Andrew Witte <andrew.witte@vrstudios.com>
2017-09-08 14:47:13 -0700

    fixed async bugs and not all services found

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Zeroconf;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var responses = await ZeroconfResolver.ResolveAsync("_services._tcp.local.");
+            foreach (var r in responses)
+            {
+                foreach (var s in r.Services)
+                {
+                    System.Diagnostics.Debug.WriteLine(s.Value.Name);
+                }
+            }
+        }
+    }
+}
